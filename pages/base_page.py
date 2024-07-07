ab06da075f2187ac1c993799176ed97f8b230f4e
Алёна Родионова <a.rodionovaa.95@gmail.com>
2024-07-07 23:40:12 +0300

    new allure decorators and comments added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,35 +1,44 @@
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from locators.base_page_locators import BasePageLocators
+import allure
 
 
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
+    @allure.step('Скроллим до элемента')
     def scroll_to_element(self, locator):
         element = self.driver.find_element(*locator)
         self.driver.execute_script('arguments[0].scrollIntoView();', element)
 
+    @allure.step('Ждем отображение элемента')
     def wait_visibility_of_element(self, locator):
         return WebDriverWait(self.driver, 25).until(expected_conditions.visibility_of_element_located(locator))
 
+    @allure.step('Кликаем на элемент')
     def click_on_element(self, locator):
         self.driver.find_element(*locator).click()
 
+    @allure.step('Вводим значение в поле для ввода')
     def send_keys_to_input(self, locator, keys):
         self.driver.find_element(*locator).send_keys(keys)
 
+    @allure.step('Получаем текст элемента')
     def get_text_of_element(self, locator):
         return self.driver.find_element(*locator).text
 
+    @allure.step('Переходим на другую вкладку')
     def switch_to_next_tab(self):
         self.driver.switch_to.window(self.driver.window_handles[1])
 
+    @allure.step('Получаем лого загруженной страницы')
     def check_visibility_of_logo(self):
         WebDriverWait(self.driver, 6).until(expected_conditions.presence_of_element_located(
             BasePageLocators.dzen_logo))
         return self.driver.title
 
+    @allure.step('Проверяем отображение элемента')
     def check_displaying_of_element(self, locator):
         return self.driver.find_element(*locator).is_displayed()
